da0a926c4b9534fe02b36b3a31e081171c17d73c
Anand Tajane <anandtajane48@gmail.com>
2025-11-22 22:47:50 +0530

    Add Contains Duplicate Solutions

---
+++ src/arrays/ContainsDuplicate.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.HashMap;
+
+// check If an array contains duplicate or not and if it contains duplicate return true or return false
+
+public class ContainsDuplicate {
+	
+	  public static void main(String args[]) {
+		  
+		   int nums[]= {1,2,3,1};
+		   
+		    System.out.println(checkDuplicate(nums));
+		    
+	  }
+	  
+	  public static boolean checkDuplicate(int nums[]) {
+		  
+		    HashMap<Integer,Integer> map = new HashMap<>();
+		    
+		       for(int i=0 ;i<nums.length;i++) {
+		    	    if(map.containsKey(nums[i])) {
+		    	    	return true;
+		    	    }
+		       
+		      map.put(nums[i],i);
+		       }
+		     return false;
+		  
+		  
+	  }
+
+}
