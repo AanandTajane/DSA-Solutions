619614049347a1dad34d2ab1d43565d8f0934b97
Anand Tajane <anandtajane48@gmail.com>
2025-11-23 23:02:50 +0530

    Add Move Zero to End

---
+++ src/arrays/MoveZeroToEnd.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class MoveZeroToEnd {
+
+	public static void main(String[] args) {
+		
+		int nums[] = {1,0,3,0,5};
+			   
+		  int j=0;
+		 for(int i=0;i<nums.length;i++) {
+			   
+			     if(nums[i] !=0 ) {
+			    	 int temp= nums[i];
+			    	 nums[i]=nums[j];
+			    	 nums[j] =temp;
+			    	 j++;
+			     }
+		   }
+		     for(int i=0; i<nums.length;i++) {
+		    System.out.println(nums[i]);
+		     }
+	}
+
+}
